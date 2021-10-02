c88d66449dc41190a0a56d6677509d2c2b2e7fe9
Brewhouse Digital <zach@brewhouse.digital>
2021-10-02 11:45:36 -0500

    Added new gallery mode and created a layout file to keep everything nice and contained

---
+++ src/routes/the-elements.svelte
@@ -0,0 +1,19 @@
+<script>
+    import PortfolioItem from "$lib/components/PortfolioItem.svelte";
+    import portfolioItems from "$lib/portfolioItems.json";
+
+    let customGallery = [
+        "carnelian.jpg",
+        "moss-agate.jpg",
+        "Iris.JPG",
+        "fountain.JPG",
+    ];
+
+    let filteredGallery = portfolioItems.filter(item => customGallery.includes(item.filename));
+</script>
+
+<h2 class="text-center fw-light mb-5 mt-3" style="letter-spacing: 4px;">The Elements</h2>
+
+{#each filteredGallery as item}
+    <PortfolioItem file={item.filename} caption={item.caption} />
+{/each}
